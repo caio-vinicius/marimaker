18969f8136f25bc12402d95733cd8f70a13d8756
rafa <rafa>
2022-01-23 11:54:30 -0300

    -sudo

--- overlay.py
+++ overlay.py
@@ -10,7 +10,7 @@ def do_the_trick(path):
 	hsize = int((float(img.size[1])*float(wpercent)))
 	img3 = img.resize((basewidth,hsize), Image.ANTIALIAS)
 	img.save("./assets/upload.png")
-	string = "sudo backgroundremover -i " + "./assets/upload.png" + " -m "+ "u2net_human_seg" + " -o ./assets/result.png"
+	string = "backgroundremover -i " + "./assets/upload.png" + " -m "+ "u2net_human_seg" + " -o ./assets/result.png"
 	os.system(string)
 	cropedImage = Image.open("./assets/result.png")
 	converter = ImageEnhance.Color(cropedImage)
