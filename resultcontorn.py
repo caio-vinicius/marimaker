0b53656bcbbdd268588184a690be830bcafa27b1
rafa <rafa>
2022-01-23 09:13:40 -0300

    mix_blend colors

--- resultcontorn.py
+++ resultcontorn.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread("result.png", cv2.IMREAD_UNCHANGED)
 
-*_, alpha = cv2.split(image)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV)
