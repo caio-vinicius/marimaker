b1da8cb0fb152db18114c158da5af468397d21d7
rafa <42rpaulino@gmail.com>
2023-02-05 20:46:21 -0300

    nice random uid and delete unused files

--- overlay.py
+++ overlay.py
@@ -2,11 +2,13 @@
 from PIL import Image, ImageEnhance
 import os
 from random import randrange
+import secrets
+import glob
 
 def do_the_trick(path):
 	img = Image.open(path)
 
-	rand = str(randrange(10000000000))
+	rand = secrets.token_hex(16)
 	basewidth = 500
 	wpercent = (basewidth/float(img.size[0]))
 	hsize = int((float(img.size[1])*float(wpercent)))
@@ -18,4 +20,7 @@ def do_the_trick(path):
 	converter = ImageEnhance.Color(cropedImage)
 	cropedImage = converter.enhance(0)
 	cropedImage.save("./images/"+rand+".png")
+	files = glob.glob('./images/*_up.png') + glob.glob('./images/*_res.png')
+	for file in files:
+		os.remove(file)
 	return rand
